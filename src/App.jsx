c9fc72a709310c80f1c5c62461882df6b229541b
Kim-kym <msgim725@gmail.com>
2024-12-20 14:45:47 +0900

    userpropscomponents

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import StyledComponents from "/src/styled/StyledComponents";
 import PropsComponents from "/src/styled/PropsComponents";
+import StyledDiv from "/src/styled/UserPropsComponents";
 
 function App() {
   return (
@@ -8,6 +9,9 @@ function App() {
       <StyledComponents />
       <h1>PropsComponents</h1>
       <PropsComponents />
+      <h1>UserPropsComponents</h1>
+      <StyledDiv dark={false} />
+      <StyledDiv dark={true} />
     </div>
   );
 }
